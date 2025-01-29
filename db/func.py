6a09ca0442e7e405a08785082d64240f906c4eb8
Athar-Naveed <asphaltlegends24@gmail.com>
2025-01-29 16:31:45 +0500

    chatbot now retrieving chats

--- db/func.py
+++ db/func.py
@@ -3,6 +3,22 @@ from sqlalchemy.orm import Session
 from type.chat_type import ChatType
 from models.auth_model import UserChats
 
+def retrieving_chats(
+        user_id: str,
+        db: Session
+        ):
+    try:
+        resp = db.query(UserChats).filter(UserChats.users == user_id).all()
+        chats = []
+        for chat in resp:
+            chats.append({"prompt": chat.user_prompt, "media_image": chat.chat_image, "bot_response": chat.bot_response})
+        return chats
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail={"message": f"Error retrieving chats: {e}"}
+        )
+
 def storing_chat(
         chat:ChatType,
         db:Session):
--- routes/chatbot.py
+++ routes/chatbot.py
@@ -8,7 +8,7 @@ import google.generativeai as genai
 from database import get_db
 from auth.utils import decoding_jwt_token 
 from bots.mobile_bot import medical_grocery_chat
-from db.func import storing_chat
+from db.func import storing_chat,retrieving_chats
 
 
 
@@ -24,6 +24,19 @@ async def get_current_user(token:Annotated[dict, Depends(oauth2_scheme)]):
     decoded_data = decoding_jwt_token(token)
     return decoded_data
 
+
+@app.get("/v1/get_chat")
+async def get_chat(
+    token: Annotated[dict, Depends(get_current_user)],
+    db: Session = Depends(get_db)
+    ):
+    try:
+        chats = retrieving_chats(token['user_id'],db=db)
+        return {"message": chats, "status": status.HTTP_200_OK}
+    except Exception as e:
+        print(f"Error: {e}")
+        return {"message": f"Error! {e}", "status": status.HTTP_500_INTERNAL_SERVER_ERROR}
+
 @app.post("/v1/extracting_items")
 async def extracting_items(
     token: Annotated[dict, Depends(get_current_user)],
@@ -36,7 +49,12 @@ async def extracting_items(
         if prompt or media_image:
             resp = await medical_grocery_chat(prompt)
             
-            chat = {"prompt": prompt,"media_image": media_image,"resp":str(resp),"user_id":token["user_id"]}
+            chat = {
+                "prompt": prompt,
+                "media_image": media_image,
+                "resp":str(resp),
+                "user_id":token["user_id"]
+                }
             store = storing_chat(chat,db=db)
             
             return {"message": resp, "status": status.HTTP_200_OK}
