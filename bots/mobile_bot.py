19b9b2fe5108bf86f972ac8c6260ea6f071ca7c2
Athar-Naveed <asphaltlegends24@gmail.com>
2025-03-17 15:43:11 +0500

    test x16

--- bots/mobile_bot.py
+++ bots/mobile_bot.py
@@ -7,18 +7,36 @@ from pathlib import Path
 client = genai.Client(api_key=os.getenv('GEMINI_API_KEY'))
 # images_url = os.getenv("DEVELOPMENT_URL")
 async def medical_grocery_chat(prompt: str,media_image:str = None):
-    
     try:
-        with open(Path("prompts/general.txt"),"r") as f:
+        if media_image and prompt == None:
+            file_ref = client.files.upload(file=media_image)
             response = client.models.generate_content(
                 model="gemini-2.0-flash",
-                contents=
-                f"""
-            Make decisions based on this: {f.read()}
-            and here is the question:
-            {prompt}"""
-                )
-        return response.text
+                contents=["Extract only the medicine or product name and power (remember mg is the power of the medicine), keep the quantity 1 (never forget). And return the contents of the image in json format",
+              file_ref]
+            )
+            return response.text
+        elif prompt and media_image:
+            file_ref = client.files.upload(file=media_image)
+            
+            response = client.models.generate_content(
+                model="gemini-2.0-flash",
+                contents=[f"{prompt}. Explain the user about the medical or grocery information provided in the image in the easiest possible manner.",
+              file_ref]
+            )
+            return response.text
+            
+        else:
+            with open(Path("prompts/general.txt"),"r") as f:
+                response = client.models.generate_content(
+                    model="gemini-2.0-flash",
+                    contents=
+                    f"""
+                Make decisions based on this: {f.read()}
+                and here is the question:
+                {prompt}"""
+                    )
+            return response.text
     except Exception as e:
         print(f"Error: {e}")
         return {"error": str(e)}  # ✅ Still a generator in case of error
